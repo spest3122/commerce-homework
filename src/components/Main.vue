51bad9ec9ccf21d91cee3e94daa7506fda837475
spest3122 <spest3122@gmail.com>
2024-02-19 22:49:17 +0800

    feature 1. 主頁切分左右兩邊 2. 引入圖片至左方

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,20 @@
 <template lang="">
-  <main class="pt-24">This is main</main>
+  <main class="pt-24 flex h-full">
+    <section class="w-1/2">
+      <img v-for="i in 3" :src="`/src/assets/picture${i}.png`" />
+    </section>
+    <section class="w-1/2 relative flex justify-center items-center">
+      <div class="fixed flex flex-col">
+        <div class="flex justify-end"><HeartIcon class="w-7 h-7" /></div>
+        <div class="flex justify-between">
+          <p>就是圖片一圖片一</p>
+          <p>NT. 100000</p>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
-<script>
-export default {};
+<script setup>
+import { HeartIcon } from "@heroicons/vue/24/outline";
 </script>
 <style lang=""></style>
