03fd784d79e2ced9f2349d39334cf150be6487f6
spest3122 <spest3122@gmail.com>
2024-02-20 23:12:58 +0800

    feature 1. 修正stickyBottom 沒有添加z-index

--- src/components/Main/StickyBottom.vue
+++ src/components/Main/StickyBottom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hidden max-lg:block fixed bottom-0 w-full">
+  <div class="hidden max-lg:block fixed bottom-0 w-full z-10">
     <button class="text-white bg-black py-3 w-full">
       {{ "加入購物車" }}
     </button>
