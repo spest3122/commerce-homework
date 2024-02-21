2b036a39aaf80648a33c2322c7b629ac9ab52843
spest3122 <spest3122@gmail.com>
2024-02-21 23:22:22 +0800

    feature 1. 加入modal.vue

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,40 @@
     <Main></Main>
     <Carousel />
     <Footer></Footer>
-    <Modal :visible="true">
-      <h2>Modal Content</h2>
-      <p>This is the content of the modal.</p>
-    </Modal>
+    <Modal
+      :modalConfig="modalConfig"
+      @close="modalConfig.visible = !modalConfig.visible"
+    ></Modal>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import Header from "./components/Header.vue";
-import Main from "./components/Main/Main.vue";
+import Main from "./components/Main/Index.vue";
 import Sidebar from "./components/Sidebar.vue";
 import Footer from "./components/Footer.vue";
 import Carousel from "./components/Carousel.vue";
 import Modal from "./components/Utils/Modal.vue";
+
 const sideBarStatus = ref(true);
+const modalConfig = ref({
+  visible: true,
+  content: `
+  <div class="w-20 h-20 bg-lime-100 mb-4"></div>
+      <p>已加入購物車</p>
+    `,
+  cancelTimeout: false,
+});
+/**
+ * status1 <div class="w-20 h-20 bg-lime-100 mb-4"></div>
+      <p>已加入購物車</p>
+      cancelTimeout: false
+ * status2 <div class="w-20 h-20 bg-lime-100 mb-4"></div>
+      <p>請選擇尺寸</p>
+      cancelTimeout: true
+
+ */
 function toggleSideBar() {
   sideBarStatus.value = !sideBarStatus.value;
 }
--- src/components/Utils/Modal.vue
+++ src/components/Utils/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="visible"
+    v-if="modalConfig.visible"
     class="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50"
   >
     <div
@@ -8,26 +8,39 @@
       @click="close"
     ></div>
     <div
-      class="bg-white p-20 rounded-3xl shadow-[0_2_5_rgba(0,0,0,0.5)] z-[60]"
+      class="bg-white p-10 min-w-[300px] rounded-3xl shadow-[0_2_5_rgba(0,0,0,0.5)] z-[60] flex flex-col justify-center items-center"
     >
-      <slot></slot>
+      <div
+        class="flex flex-col items-center justify-center"
+        v-html="modalConfig.content"
+      ></div>
+      <button
+        v-if="modalConfig.cancelTimeout"
+        class="border hover:bg-amber-100 hover:border-amber-100 w-full p-4"
+        @click="$emit('close')"
+      >
+        確定
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-defineProps({
-  visible: {
-    type: Boolean,
-    default: false,
-  },
+import { onMounted } from "vue";
+const props = defineProps({
+  modalConfig: Object,
 });
+const emit = defineEmits(["close"]);
 
-const close = () => {
-  console.log(1);
-  //   emit("visible");
-};
+const defaultTimeout = 2500;
+
+onMounted(() => {
+  if (!props.modalConfig.cancelTimeout) {
+    setTimeout(() => {
+      emit("close");
+    }, defaultTimeout);
+  }
+});
 </script>
 
 <style scoped>
